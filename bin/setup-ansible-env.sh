bcb7c021a3d6780a08c0ab3197a1997f1652f8ff
Nick Winn <technick@gmail.com>
2016-06-13 13:54:03 -0600

    added some variables to the setup file

--- bin/setup-ansible-env.sh
+++ bin/setup-ansible-env.sh
@@ -3,4 +3,5 @@ REPO_BASE=$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )
 
 # Set Ansible Inventory (can be overridden by plays, cmdline, etc.)
 export ANSIBLE_HOSTS=${REPO_BASE}/inventory
-
+export PYTHONPATH=~/ansible/lib
+export MANPATH=~/ansible/docs/man
